5a45e5e37ea1984f11bb0bb6c0b5aed1cee235ef
Shirley <1041264719@qq.com>
2018-10-10 10:14:07 +0800

    python

--- scratch.py
+++ scratch.py
@@ -1519,4 +1519,95 @@ import functools
 #     print('finally...')
 # print('END')
 
+# try:
+#     foo()
+# except ValueError as e:
+#     print('ValueError')
+# except UnicodeError as e:
+#     print('UnicodeError')
 
+# def foo(s):
+#     return 10 / int(s)
+#
+# def bar(s):
+#     return foo(s) * 2
+#
+# def main():
+#     try:
+#         bar('0')
+#     except Exception as e:
+#         print('Error:', e)
+#     finally:
+#         print('finally...')
+#
+# def foo(s):
+#     return 10 /int(s)
+#
+# def bar(s):
+#     return foo(s) * 2
+#
+# def main():
+#     bar('0')
+
+# import logging
+#
+# def foo(s):
+#     return 10 / int(s)
+#
+# def bar(s):
+#     return foo(s) * 2
+#
+# def main():
+#     try:
+#         bar('0')
+#     except Exception as e:
+#         logging.exception(e)
+#
+# main()
+# print('END')
+
+# class FooError(ValueError):
+#     pass
+#
+# def foo(s):
+#     n = int(s)
+#     if n == 0:
+#         raise FooError('invalid value: %s' %s)
+#     return 10 / n
+#
+# foo(0)
+
+# def foo(s):
+#     n = int(s)
+#     if n == 0:
+#         raise ValueError('invaild value: %s' %s)
+#     return 10 / n
+#
+# def bar():
+#     try:
+#         foo('0')
+#     except ValueError as e:
+#         print('ValueError!')
+#         raise
+
+# from functools import reduce
+#
+# def str2num(s):
+#     isF1o = s.find('.')
+#     if isF1o == -1:
+#         return int(s)
+#     else:
+#         return float(s)
+#
+# def calc(exp):
+#     ss = exp.split('+')
+#     ns = map(str2num, ss)
+#     return reduce(lambda acc, x: acc + x, ns)
+#
+# def main():
+#     r = calc('100 + 200 + 345')
+#     print('100 + 200 + 345 =', r)
+#     r = calc('99 + 88 + 7.6')
+#     print('99 + 88 + 7.6 =', r)
+#
+# main()
